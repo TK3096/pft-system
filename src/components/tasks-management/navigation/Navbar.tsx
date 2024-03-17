7e7b4a65ca232db89e8a824b4b4aeefc1dcfd862
tongthanaphon <tongthanaphon.tk@gmail.com>
2024-03-17 18:28:26 +0700

    update: navbar

--- src/components/tasks-management/navigation/Navbar.tsx
+++ src/components/tasks-management/navigation/Navbar.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { TaskState } from '@/types'
+
 import { useParams } from 'next/navigation'
 
 import { Hash } from 'lucide-react'
@@ -7,11 +9,12 @@ import { Hash } from 'lucide-react'
 import { useTasksManagement } from '@/hooks/useTasksManagement'
 
 import { AddButton } from '@/components/tasks-management/AddButton'
+import { ActionTooltip } from '@/components/common/ActionTooltip'
 
 export const Navbar = () => {
   const params = useParams()
 
-  const { boards } = useTasksManagement({
+  const { boards, tasks } = useTasksManagement({
     boardId: params.boardId as string,
   })
 
@@ -23,12 +26,54 @@ export const Navbar = () => {
 
   const board = boards[0]
 
+  const todos = tasks.filter((task) => task.state === TaskState.TODO)
+  const inProgress = tasks.filter(
+    (task) => task.state === TaskState.IN_PROGRESS,
+  )
+  const reviews = tasks.filter((task) => task.state === TaskState.REVIEW)
+  const done = tasks.filter((task) => task.state === TaskState.DONE)
+
   return (
     <div className='h-full w-full px-5 flex items-center dark:bg-neutral-800 shadow-md'>
       <div className='flex items-center gap-x-7'>
         <div className='flex items-center gap-x-3 font-semibold'>
           <Hash className='w-6 h-6' />
           <h3>{board.name}</h3>
+        </div>
+
+        <div className='flex items-center gap-x-3'>
+          <div className='flex items-center gap-1'>
+            <p className='font-bold mr-1'>{tasks.length}</p>
+            <p className='font-bold'>Tasks</p>
+          </div>
+
+          <ActionTooltip label='todo' side='bottom' align='center'>
+            <div className='flex items-center gap-1'>
+              <div className='h-2 w-2 rounded bg-zinc-400' />
+              <p className='font-semibold'>{todos.length}</p>
+            </div>
+          </ActionTooltip>
+
+          <ActionTooltip label='in progress' side='bottom' align='center'>
+            <div className='flex items-center gap-1'>
+              <div className='h-2 w-2 rounded bg-yellow-600' />
+              <p className='font-semibold'>{inProgress.length}</p>
+            </div>
+          </ActionTooltip>
+
+          <ActionTooltip label='review' side='bottom' align='center'>
+            <div className='flex items-center gap-1'>
+              <div className='h-2 w-2 rounded bg-indigo-400' />
+              <p className='font-semibold'>{reviews.length}</p>
+            </div>
+          </ActionTooltip>
+
+          <ActionTooltip label='done' side='bottom' align='center'>
+            <div className='flex items-center gap-1'>
+              <div className='h-2 w-2 rounded bg-green-400' />
+              <p className='font-semibold'>{done.length}</p>
+            </div>
+          </ActionTooltip>
         </div>
 
         <AddButton label='new task' type='task' />
