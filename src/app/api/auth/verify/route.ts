dffabed15ac2e4e578995c9853a537b241bfb07b
tongthanaphon <tongthanaphon.tk@gmail.com>
2024-05-06 23:49:41 +0700

    fix: auth

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -1,31 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 import { logout } from '@/lib/firebase/auth'
-import { getCurrentUser, revokeAllSessions } from '@/lib/firebase-sdk/auth'
+import { getCurrentUser } from '@/lib/firebase-sdk/auth'
 import { SESSION_KEY } from '@/lib/constant'
 
-export const POST = async () => {
+export const POST = async (req: NextRequest) => {
   const user = await getCurrentUser()
 
   if (user) {
-    return Response.json({ status: true })
+    return NextResponse.json({ status: true })
   }
 
   const sessionCookie = cookies().get(SESSION_KEY)?.value
 
   if (!sessionCookie) {
-    return Response.json({ status: false, error: 'No session found' })
+    return NextResponse.json({ status: false })
   }
 
-  const result = await logout()
-
-  if (!result) {
-    return Response.json({ status: false, error: 'Fail to logout' })
-  }
-
+  await logout()
   cookies().delete(SESSION_KEY)
 
-  await revokeAllSessions(sessionCookie)
-
-  return Response.redirect('/auth/login')
+  return NextResponse.json({ status: false })
 }
--- src/components/auth/CheckAuth.tsx
+++ src/components/auth/CheckAuth.tsx
@@ -1,17 +1,27 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+
 import { useEffect } from 'react'
 
 export const CheckAuth = () => {
+  const router = useRouter()
+
   useEffect(() => {
     const auth = async () => {
-      await fetch('/api/auth/verify', {
+      const response = await fetch('/api/auth/verify', {
         method: 'POST',
       })
+
+      const json = await response.json()
+
+      if (response.ok && !json.status) {
+        router.refresh()
+      }
     }
 
     auth()
-  }, [])
+  }, [router])
 
   return null
 }
